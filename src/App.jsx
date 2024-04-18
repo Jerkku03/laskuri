a783c6ee05689289e62578b1b96c64b4a6c2f7ff
Jerkku03 <jere.hietikko@live.fi>
2024-04-18 21:05:05 +0300

    materiaalien lisäys formi

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,86 @@
+import { useState } from 'react'
 
-
+//materiaali vaihtoehdot
 const Materiaalit = () => {
+
+  const lastulevy = 'LASTULEVY 11 P1 11X1200X2600 MM POHJAMAALATTU 3,12 M²'
+  const kestopuu = 'KESTOPUU HÖYLÄTTY RAW AB 28X120 MM RUSKEA VIISTETTY'
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('toimii');
+  }
+
   return (
-    <select name='materiaalit'>
-        <option value='betoni'>KESTOPUU HÖYLÄTTY RAW AB 28X120 MM RUSKEA VIISTETTY</option>
-        <option value="laminaatti">laminaatti</option>
-        <option value="naulapaketti">naulapaketti</option>
-      </select>
+      <form onSubmit={handleSubmit} id='sisalto'>
+        <p>materiaali:</p>
+        <select name='materiaalit'>
+            <option value="lastulevy">{lastulevy}</option>
+            <option value='kestopuu'>{kestopuu}</option>
+            <option value="naulapaketti">naulapaketti</option>
+          </select>
+          <p>kpl:</p>
+          <input type="number"/>
+          <input type="submit" />
+        </form>
   )
 }
 
-const Lisaa = () => {
+//lisää materiaalin listaan
+const Lisaa = (props) => {
+   props.lista.push(Materiaalit.value)
+}
+  
+
+//kaikki lisätyt materiaalit
+const Lisatut = (props) => {
+  let lista = props.lista
+
+  if (lista.length > 0){
+    return (
+      <div>
+        <p>löytyy</p>
+      </div>
+    )
+  }
   return (
-    <button>lisää</button>
+    <div>
+      <p>Aloita lisäämään materiaaleja</p>
+    </div>
   )
+
 }
 
+//tallentaa pdf tiedoston materiaaleista
 const Tulosta = () => {
   return (
-    <button>Tulosta PDF</button>
+    <button>Tallenna PDF</button>
   )
 }
 
 
 
-const App = () => (
+const App = () => {
+
+  const lista = {}
+
+  return (
   <div id="kokosivu">
     <h1>Laske rakentamisessa käytettävien materialien päästöt</h1>
 
     <div id="sisalto">
-      <p>materiaali:</p>
       <Materiaalit/>
-      <p>kpl:</p>
-      <input type="integer" />
-      <Lisaa/>
     </div>
+
+    <div>
+      <Lisatut lista={lista}/>
+    </div>
+
     <div>
       <Tulosta/>
     </div>
-
   </div>
-)
+  )
+}
 
 export default App
