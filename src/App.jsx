fe222922c34d161ea47357a0046e90094d16f03f
Jerkku03 <jere.hietikko@live.fi>
2024-04-19 15:21:52 +0300

    lisaa materiaalin ja maaran sanakirjaan

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
 import { useState } from 'react'
-import { FormProvider, useForm } from 'react-hook-form'
 
 //materiaali vaihtoehdot
-const Materiaalit = () => {
+const Materiaalit = (props) => {
 
-  const lastulevy = 'LASTULEVY 11 P1 11X1200X2600 MM POHJAMAALATTU 3,12 M²'
-  const kestopuu = 'KESTOPUU HÖYLÄTTY RAW AB 28X120 MM RUSKEA VIISTETTY'
+  var lastulevy = {'materiaali' : 'LASTULEVY 11 P1 11X1200X2600 MM POHJAMAALATTU 3,12 M²',
+                      'hinta': 50}
+  var kestopuu = {'materiaali' : 'KESTOPUU HÖYLÄTTY RAW AB 28X120 MM RUSKEA VIISTETTY',
+                    'hinta' : 40}
+  
+  var lista = props.lista
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(document.getElementById('materiaalit').value)
-    console.log(document.getElementById('numero').value)
+    
+    //lisää materiaali listaan
+
+    props.tila([
+      ...lista,
+      {'materiaali' : document.getElementById('materiaalit').value ,
+      'maara' : document.getElementById('numero').value}
+    ]);
+
+    document.getElementById('materiaalit').value = '';
+    document.getElementById('numero').value = '';
+
   }
 
   return (
       <form id='sisalto'>
           <label htmlFor='materiaalit'>materiaali:</label>
           <select name='materiaalit' label='materiaali:' id='materiaalit'>
-              <option>{lastulevy}</option>
-              <option>{kestopuu}</option>
+              <option>{lastulevy['materiaali']}</option>
+              <option>{kestopuu['materiaali']}</option>
             </select>
             <label htmlFor='numero'>kpl:</label>
             <input id='numero' name='numero' type='number' label='numero' required/>
@@ -26,21 +39,20 @@ const Materiaalit = () => {
         </form>
   )
 }
-
-//lisää materiaalin listaan
-const Lisaa = (props) => {
-   props.lista.push(Materiaalit.value)
-}
   
 
 //kaikki lisätyt materiaalit
 const Lisatut = (props) => {
+
   let lista = props.lista
 
+  console.log(lista.length)
+
   if (lista.length > 0){
+    const listItems = lista.map((d) => <li key={d.materiaali}>{d.materiaali}       kpl: {d.maara}</li>);
     return (
       <div>
-        <p>löytyy</p>
+        <p>{listItems}</p>
       </div>
     )
   }
@@ -64,6 +76,7 @@ const Tulosta = () => {
 const App = () => {
 
   const [lista, setLista] = useState([])
+ 
 
   return (
   <div id="body">
@@ -77,7 +90,7 @@ const App = () => {
     <h1>Laske rakentamisessa käytettävien materialien päästöt</h1>
 
     <div id="sisalto">
-      <Materiaalit/>
+      <Materiaalit lista={lista} tila={setLista}/>
     </div>
 
     <div>
