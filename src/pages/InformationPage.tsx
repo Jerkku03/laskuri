31303603594d2020281586c8e643ae301a70cd0d
Jerkku03 <108393599+Jerkku03@users.noreply.github.com>
2025-07-04 17:14:01 +0300

    kuva src kansioon

--- src/pages/InformationPage.tsx
+++ src/pages/InformationPage.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import photo from "../paastolaskuri.png"
 
 const InformationPage = () => {
     return (
         <div>
             <h3>miltä ohjelmisto näyttää:</h3>
-            <img className="firstPic" src="../päästölaskuri.png" alt="image"/>
+            <img className="firstPic" src={photo} alt="image"/>
         </div>
     )
 }
